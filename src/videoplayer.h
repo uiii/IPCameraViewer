94cdd8824dad7223dde420d79e1b05c94d5b77e8
Richard Jedlička <jedlicka.r@gmail.com>
2014-11-23 17:07:24 +0100

    add license

--- src/videoplayer.h
+++ src/videoplayer.h
@@ -1,3 +1,11 @@
+/*
+ * This file is part of IPCameraViewer project (http://github.com/uiii/IPCameraViewer)
+ * Copyright (c) 2014 Richard Jedlička <jedlicka.r@gmail.com> (http://uiii.cz)
+ *
+ * This software may be modified and distributed under the terms of the MIT license.
+ * See the LICENSE file (http://github.com/uiii/IPCameraViewer/LICENSE) for details.
+ */
+
 #ifndef VIDEOPLAYER_H
 #define VIDEOPLAYER_H
 
--- src/vlcvideoplayer.cpp
+++ src/vlcvideoplayer.cpp
@@ -1,3 +1,11 @@
+/*
+ * This file is part of IPCameraViewer project (https://github.com/uiii/IPCameraViewer)
+ * Copyright (c) 2014 Richard Jedlička <jedlicka.r@gmail.com> (http://uiii.cz)
+ *
+ * This software may be modified and distributed under the terms of the MIT license.
+ * See the LICENSE file (https://github.com/uiii/IPCameraViewer/LICENSE) for details.
+ */
+
 #include "vlcvideoplayer.h"
 
 #include <vlc/vlc.h>
